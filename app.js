928d447ccbf320df42e9d9620f8923ca2f4fd2a9
errorr404 <dkbishwash@gmail.com>
2018-07-24 00:39:29 +0530

    Wiring up with weather search by API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // here we import the request module:- https://www.npmjs.com/package/request
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
+const request = require('request');
 
 const argv = yargs
           .options({
@@ -24,5 +25,29 @@ const argv = yargs
       }
       else {
         console.log(JSON.stringify(result,undefined,2));
-      }
-    });
+      //  console.log(`https://api.darksky.net/forecast/5cfc84e0ce6384cf83b839aedd5668cd/${result.Latitude},${result.Longitude}`);
+        request({
+
+          url:`https://api.darksky.net/forecast/5cfc84e0ce6384cf83b839aedd5668cd/${result.Latitude},${result.Longitude}`,
+
+          json: true
+        },(error,response,body) => {
+          if(error) {
+            console.log('Unable to connect to the forecast.io server');
+          }
+          else if(response.statusCode === 400)
+          {
+            console.log('Unable to fetch Weather');
+          }
+          else if(response.statusCode === 200)
+          {
+            console.log(body.currently.temperature);
+          }
+
+
+      });
+    }
+  });
+
+  // 5cfc84e0ce6384cf83b839aedd5668cd
+  //https://api.darksky.net/forecast/[key]/[latitude],[longitude]
